156ca4f59b6b8170a71772e53fc0de910998e01f
Luis <seivaneluis@gmail.com>
2025-05-26 20:55:22 -0300

    Se agrego el contador de intentos al programa principal.

--- juego.py
+++ juego.py
@@ -3,14 +3,19 @@ import random
 def jugar():
     numero_secreto = random.randint(1, 100)
     adivinado = False
+    # Adherido Isuss intentos.
+    intentos = 0
 
     print("¡Bienvenido al  juego del número secreto!")
     print("Adivina el número del 1 al 100")
 
     while not adivinado:
         intento = int(input("Ingresa un número: "))
+        # Adherido Isuss intentos.
+        intentos = intentos + 1
         if intento == numero_secreto:
-            print("¡Felicidades! Adivinaste el número.")
+            # Adherido Isuss intenros en print.
+            print(f"¡Felicidades! Adivinaste el número en {intentos} intentos.")
             adivinado = True
         elif intento < numero_secreto:
             print("El número es mayor.")
